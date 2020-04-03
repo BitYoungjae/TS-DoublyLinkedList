819e6d830ca88b3da35dde60cb3090fd29812a55
BitYoungjae <bityoungjae@gmail.com>
2020-04-04 02:46:36 +0900

    add simple test : list.swap

--- test.js
+++ test.js
@@ -8,10 +8,15 @@ list.insert(3432, 0, 'front');
 const secondTest = list.search(3432);
 const thirdTest = list.size;
 
+const list2 = new LinkedList(1, 2, 3, 4);
+list2.swap(1, 3);
+const fourthTest = '' + list2.get(1) + list2.get(3);
+
 console.log(
   `
   #1 : ${firstTest === 4}
   #2 : ${secondTest === 0}
   #3 : ${thirdTest === 6}
+  #4 : ${fourthTest === '42'}
   `,
 );
